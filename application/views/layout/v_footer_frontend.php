0498eb1aab208119902678cb74ab03eafc754a15
Heycal14 <heycal30@gmail.com>
2021-03-26 09:45:05 +0700

    First Commit

---
+++ application/views/layout/v_footer_frontend.php
@@ -0,0 +1,30 @@
+</div>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<!-- Main Footer -->
+<footer class="main-footer fixed-bottom">
+  <!-- To the right -->
+  <div class="float-right d-none d-sm-inline">
+    Anything you want
+  </div>
+  <!-- Default to the left -->
+  <strong>Copyright &copy; 2020-2021 <a href="https://adminlte.io">Fly Code</a>.</strong> All rights reserved.
+</footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+
+</body>
+<script>
+  window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function() {
+      $(this).remove();
+    });
+  }, 3000)
+</script>
+
+</html>
